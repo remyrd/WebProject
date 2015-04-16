ddb4c790c069b96e954f63feaf3ae3f290186aa5
Remy Rojas <rrojasdela92@gmail.com>
2015-04-16 14:02:54 +0300

    trying to move to gevent server, first step: become independent from flask-script

--- db_adduser.py
+++ db_adduser.py
@@ -3,13 +3,12 @@ from manage import app
 from app import db
 from app.models import User
 from getpass import getpass
-
+import sys
 
 password = getpass()
 password2= getpass()
 if password2 != password:
-	import sys
 	sys.exit("passwords don\'t match")
-new_user=User(username=argv[1], email= argv[1]+"@pwebproject.org", password=password)
+new_user=User(username=sys.argv[1], email= sys.argv[1]+"@pwebproject.org", password=password)
 db.session.add(new_user)
 db.session.commit()
