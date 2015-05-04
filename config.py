44517d8e131926c0dac35db1b6c1f3c28ff06bfe
Remy Rojas <rrojasdela92@gmail.com>
2015-05-04 10:29:41 +0300

    sockets working but not on heroku

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 class Config:
@@ -9,15 +15,15 @@ class Config:
 class DevelopmentConfig(Config):
 	DEBUG = True
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'garbage'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-	SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'data-dev.sqlite')
+	SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repo')
 class TestingConfig(Config):
 	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'data-test.sqlite')
 class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
 	DEBUG = True
-	SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repo')
+	SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repo')
 	
 config = {
 	'development': DevelopmentConfig,
